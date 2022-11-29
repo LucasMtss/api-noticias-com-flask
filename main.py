caa405e91033db97a5162b8dcdf764b9f1c736c1
Lucas Matos <54159333+LucasMtss@users.noreply.github.com>
2022-11-29 15:09:13 -0300

    feat: add error handler

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ import io
 app = Flask(__name__)
 
 def getNews(page):
-    res = requests.get(f'https://www.ifsudestemg.edu.br/noticias/barbacena/?b_start:int={int(page)*20}')
+    try:
+        res = requests.get(f'https://www.ifsudestemg.edu.br/noticias/barbacena/?b_start:int={int(page)*20}')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        # print(soup.prettify())
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # print(soup.prettify())
-
-    titles = soup.find_all("a", {"class": "summary url"})
-    descriptions = soup.find_all("span", {"class": "description"})
+        titles = soup.find_all("a", {"class": "summary url"})
+        descriptions = soup.find_all("span", {"class": "description"})
 
 
 
-    json = {
-        "noticias": []
-    }
+        json = {
+            "noticias": []
+        }
 
-    for noticia in range(len(titles)):
-        json["noticias"].append({
-            "titulo": titles[noticia].contents[0],
-            "descricao" : descriptions[noticia].contents[0]
-        })
-    
-    return json
+        for noticia in range(len(titles)):
+            json["noticias"].append({
+                "titulo": titles[noticia].contents[0],
+                "descricao" : descriptions[noticia].contents[0]
+            })
+        
+        return json
+    except:
+        return False
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def api():
@@ -43,12 +45,19 @@ def api():
 @app.route('/noticias/<pagina>', methods=['GET'])
 def noticias(pagina):
     if request.method == 'GET':
-        return json.dumps(
-            {
-                "payload": getNews(pagina),
-                "status": 200
-            }
-        )
+        response = getNews(pagina)
+        if response == False:
+            return json.dumps({
+                "status": 400,
+                "mensagem": "Página inválida"
+            })
+        else:
+            return json.dumps(
+                {
+                    "payload": getNews(pagina),
+                    "status": 200
+                }
+            )
 
 if __name__ == '__main__':
     app.run(debug=True)
